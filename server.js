1a9c82856fda1c00b2cd3588c41befba0135e56b
ShivamMalhotra1994 <mshivam@iitk.ac.in>
2020-04-22 21:17:48 +0530

    Update Server.js

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 var hash = require("hash.js");
 
-const app = express();
+const server = express();
 
-app.use(bodyParser.json({ type: "application/json" }));
+server.use(bodyParser.json({ type: "application/json" }));
 
-app.post("/hash", (req, res, next) => {
+server.post("/hash", (req, res, next) => {
   console.log(req.body);
   var ans = hash.sha256().update(req.body.data).digest("hex");
   console.log(ans);
-  res.end(JSON.stringify({ hash: ans }));
+  res.json(JSON.parse(JSON.stringify({ hash: ans })));
 });
 
-app.listen(8787);
+server.listen(8787);
